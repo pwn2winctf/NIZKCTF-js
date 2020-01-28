69dff65b3e3dbeb782597f227096052e9eb03766
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-01-28 00:01:48 -0300

    Refactor the use of Challenges

--- src/index.js
+++ src/index.js
@@ -4,19 +4,21 @@ import { Challenges } from './challenges'
 const repo = new GitHub(process.env.TOKEN)
 this.challenges = new Challenges(repo, 'pwn2winctf/2019')
 
-this.challenges
-  .list()
-  .then(list => {
-    list.forEach(challenge => {
-      console.log(challenge.name)
-      const languages = Object.keys(challenge.descriptions)
-      languages.forEach((lang, idx) => {
-        if (idx + 1 < languages.length) {
-          console.log(' ', String.fromCharCode(0x2523), lang)
-        } else {
-          console.log(' ', String.fromCharCode(0x2517), lang)
-        }
-      })
+const test = async () => {
+  const list = await this.challenges.list()
+  list.forEach(challenge => {
+    console.log(challenge.name)
+    const languages = Object.keys(challenge.descriptions)
+    languages.forEach((lang, idx) => {
+      if (idx + 1 < languages.length) {
+        console.log(' ', String.fromCharCode(0x2523), lang)
+      } else {
+        console.log(' ', String.fromCharCode(0x2517), lang)
+      }
     })
   })
-  .catch(err => console.log(err))
+  const challenge = this.challenges.get('vamos_pra_russia', 'zn')
+  console.log('selected challenge', challenge)
+}
+
+test()
