5b5381837d2780a73b660a8ea92c029caf636182
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-16 19:12:15 -0300

    Install vue-material

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
+import VueMaterial from "vue-material";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(VueMaterial);
 
 new Vue({
   router,
