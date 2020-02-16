4d1ef74559117cb8cd7a407ad1c0e5cd11883c24
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-16 17:19:15 -0300

    Fix lint and public path

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  created: function () {
-    // `this` aponta para a instância
-    window.fetch('/README.md').then(r => console.log(r))
-    console.log('a é: ')
-  },
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
