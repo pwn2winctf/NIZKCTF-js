44c218dea6e33944463cd446a96e4aab47766374
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-17 21:54:02 -0300

    Create API service

---
+++ frontend/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import config from "@/config.json";
+
+const api = axios.create({
+  baseURL: config.submissionsBasePath
+});
+
+export const API = {
+  listNews: () => api.get("/news.json")
+};
