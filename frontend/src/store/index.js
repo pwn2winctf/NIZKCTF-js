081f31d1b8c7483dd4ce2be6058ead6cc1f47f30
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-19 00:21:38 -0300

    Configure store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,36 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
+import VuexPersist from "vuex-persist";
+
+const DEV = process.env.NODE_ENV !== "production";
 
 Vue.use(Vuex);
 
+const vuexLocalStorage = new VuexPersist({
+  key: "pw2winctf",
+  storage: window.localStorage
+});
+
+const logger = createLogger();
+
+const plugins = [DEV && logger, vuexLocalStorage.plugin];
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  strict: DEV,
+  state: {
+    theme: "default"
+  },
+  mutations: {
+    setTheme(state, theme) {
+      state.theme = theme;
+    }
+  },
+  actions: {
+    setTheme(context, theme) {
+      context.commit("setTheme", theme);
+    }
+  },
+  modules: {},
+  plugins
 });
