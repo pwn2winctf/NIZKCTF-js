220d28a15369f474d1cb439e7c1c6b13ea8c311a
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-19 21:27:00 -0300

    Add the function to create pooling

---
+++ frontend/src/utils.js
@@ -0,0 +1,20 @@
+export const createPooling = (callback, milliseconds = 60000) => {
+    let interval;
+    return {
+        running: false,
+        async start() {
+            if (this.running) {
+                return;
+            }
+
+            this.running = true;
+
+            callback();
+            interval = setInterval(() => callback(), milliseconds);
+        },
+        stop() {
+            this.running = false
+            clearInterval(interval);
+        }
+    }
+};
