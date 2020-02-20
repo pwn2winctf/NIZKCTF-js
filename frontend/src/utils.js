fb329bdc884e1011634ec51b2e1b755609a63f09
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-19 21:34:48 -0300

    Fix lint

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,20 +1,20 @@
 export const createPooling = (callback, milliseconds = 60000) => {
-    let interval;
-    return {
-        running: false,
-        async start() {
-            if (this.running) {
-                return;
-            }
+  let interval;
+  return {
+    running: false,
+    async start() {
+      if (this.running) {
+        return;
+      }
 
-            this.running = true;
+      this.running = true;
 
-            callback();
-            interval = setInterval(() => callback(), milliseconds);
-        },
-        stop() {
-            this.running = false
-            clearInterval(interval);
-        }
+      callback();
+      interval = setInterval(() => callback(), milliseconds);
+    },
+    stop() {
+      this.running = false;
+      clearInterval(interval);
     }
+  };
 };
