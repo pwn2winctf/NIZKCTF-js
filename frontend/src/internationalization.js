5a5212d20fe0596539f538b11ca45639a45941cb
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-22 23:05:08 -0300

    Add file with translations

---
+++ frontend/src/internationalization.js
@@ -0,0 +1,24 @@
+export default {
+  en: {
+    language: "Language",
+    label: "English",
+    router: {
+      Home: "Home",
+      Challenges: "Challenges",
+      Rank: "Rank",
+      Rules: "Rules",
+      Settings: "Settings"
+    }
+  },
+  ptBr: {
+    language: "Idioma",
+    label: "Português (BR)",
+    router: {
+      Home: "Início",
+      Challenges: "Desafios",
+      Rank: "Rank",
+      Rules: "Regras",
+      Settings: "Configurações"
+    }
+  }
+};
