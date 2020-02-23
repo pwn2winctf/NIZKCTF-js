4306d47a0ed2acbf0b0f1087134d6cc4d5ff5fcf
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-23 02:47:53 -0300

    Fix lint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -10,8 +10,12 @@ const api = axios.create({
 export const API = {
   listNews: () => api.get("/news.json"),
   listSolvedChallenges: () => api.get("/accepted-submissions.json"),
-  getChallenges: () => api.get("/challenges/index.json", { baseURL: "https://pwn2.win/2019/" }),// TODO remove baseURL
-  getChallenge: (challenge) => api.get(`/challenges/${challenge}.json`, { baseURL: "https://pwn2.win/2019/" }),// TODO remove baseURL
+  getChallenges: () =>
+    api.get("/challenges/index.json", { baseURL: "https://pwn2.win/2019/" }), // TODO remove baseURL
+  getChallenge: challenge =>
+    api.get(`/challenges/${challenge}.json`, {
+      baseURL: "https://pwn2.win/2019/"
+    }), // TODO remove baseURL
   getTeam: teamName => {
     const path = getTeamPath(teamName);
     return api.get(`/${path}/team.json`);
