ab2ff96caf0920b2151644cbfeaa575c602adcc0
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-23 02:47:32 -0300

    Add api handler to get challenges

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -10,6 +10,8 @@ const api = axios.create({
 export const API = {
   listNews: () => api.get("/news.json"),
   listSolvedChallenges: () => api.get("/accepted-submissions.json"),
+  getChallenges: () => api.get("/challenges/index.json", { baseURL: "https://pwn2.win/2019/" }),// TODO remove baseURL
+  getChallenge: (challenge) => api.get(`/challenges/${challenge}.json`, { baseURL: "https://pwn2.win/2019/" }),// TODO remove baseURL
   getTeam: teamName => {
     const path = getTeamPath(teamName);
     return api.get(`/${path}/team.json`);
