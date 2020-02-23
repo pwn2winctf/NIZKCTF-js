ceec39764d7eef81f4cc975a140f2963ff407579
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-22 23:19:49 -0300

    Add option to change language

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,26 +1,52 @@
 <template>
-  <div>
-    <md-switch :value="this.theme === 'default'" @change="toggleTheme"
-      >Dark mode</md-switch
-    >
-  </div>
+  <md-content>
+    <md-field>
+      <label for="language">{{$t("language")}}</label>
+      <md-select
+        v-model="$i18n.locale"
+        :value="this.language"
+        name="movie"
+        id="language"
+        @md-selected="this.changeLanguage"
+      >
+        <md-option
+          v-for="item in languages"
+          :key="item.language"
+          :value="item.language"
+        >{{item.label}}</md-option>
+      </md-select>
+    </md-field>
+    <md-switch :value="this.theme === 'default'" @change="toggleTheme">Dark mode</md-switch>
+  </md-content>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
 
+import messages from "@/internationalization";
+
 export default {
   name: "Settings",
+  data: () => ({
+    languages: Object.keys(messages).map(item => ({
+      language: item,
+      label: messages[item].label
+    }))
+  }),
   computed: {
     ...mapState({
-      theme: state => state.theme
+      theme: state => state.theme,
+      language: state => state.language
     })
   },
   methods: {
-    ...mapActions(["setTheme"]),
+    ...mapActions(["setTheme", "setLanguage"]),
     toggleTheme(value) {
       const theme = value ? "dark" : "default";
       this.setTheme(theme);
+    },
+    changeLanguage(language) {
+      this.setLanguage(language);
     }
   }
 };
