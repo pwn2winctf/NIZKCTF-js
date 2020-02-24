2dfbcb8d7dbe797c169c32d48c0ffc4558bda26b
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-02-24 18:19:05 -0300

    Add filters

--- frontend/src/views/Challenges.vue
+++ frontend/src/views/Challenges.vue
@@ -1,28 +1,44 @@
 <template>
-  <md-content class="container">
-    <md-card
-      v-for="challenge in challenges"
-      v-bind:key="challenge.id"
-      class="md-primary"
-    >
-      <md-card-header>
-        <md-card-header-text>
-          <div class="md-title">{{ challenge.title }}</div>
-          <div class="md-subhead">
-            {{ $t("solves") }}: {{ challenge.solves }}
-          </div>
-          <div class="md-subhead">
-            {{ $t("score") }}: {{ challenge.points }}
-          </div>
-        </md-card-header-text>
-      </md-card-header>
+  <md-content>
+    <md-content class="chips-container">
+      <md-chip
+        v-for="item in categories"
+        :key="item"
+        :class="item === selectedCategory ? 'md-primary' : 'md-accent'"
+        md-clickable
+        @click="filterChallenges(item)"
+        >{{ item }}</md-chip
+      >
+    </md-content>
+    <div class="cards-container">
+      <md-card
+        v-for="challenge in selectedChallenges"
+        v-bind:key="challenge.id"
+        class="md-accent"
+      >
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">{{ challenge.title }}</div>
+            <div class="md-subhead">
+              {{ $t("solves") }}: {{ challenge.solves }}
+            </div>
+            <div class="md-subhead">
+              {{ $t("score") }}: {{ challenge.points }}
+            </div>
+          </md-card-header-text>
+        </md-card-header>
 
-      <md-card-actions>
-        <md-chip v-for="tag in challenge.tags" v-bind:key="tag">{{
-          tag
-        }}</md-chip>
-      </md-card-actions>
-    </md-card>
+        <md-card-actions>
+          <md-chip
+            v-for="tag in challenge.tags"
+            v-bind:key="tag"
+            class="md-primary"
+          >
+            {{ tag }}
+          </md-chip>
+        </md-card-actions>
+      </md-card>
+    </div>
   </md-content>
 </template>
 
@@ -34,7 +50,9 @@ export default {
   name: "Challenges",
   data: () => ({
     challenges: [],
-    categories: []
+    selectedChallenges: [],
+    categories: [],
+    selectedCategory: undefined
   }),
   created() {
     API.getChallenges()
@@ -44,12 +62,14 @@ export default {
       })
       .then(list => {
         const datas = list.map(({ data }) => data);
-        const categories = datas.reduce((obj, item) => {
+        const categories = datas.reduce((list, item) => {
           item.tags.forEach(tag => {
-            obj[tag] ? obj[tag].push(item.id) : (obj[tag] = [item.id]);
+            const category = list.find(item => item === tag);
+
+            !category && list.push(tag);
           });
-          return obj;
-        }, {});
+          return list;
+        }, []);
 
         this.categories = categories;
 
@@ -67,15 +87,36 @@ export default {
               points: computeScore(solves[item.id])
             }));
             this.challenges = challenges;
+            this.selectedChallenges = this.challenges;
           })
           .catch(err => console.error(err));
       });
+  },
+  methods: {
+    filterChallenges(category) {
+      if (this.selectedCategory == category) {
+        this.selectedCategory = this.undefined;
+        this.selectedChallenges = this.challenges;
+        return;
+      }
+
+      this.selectedCategory = category;
+      this.selectedChallenges = this.challenges.filter(item =>
+        item.tags.includes(category)
+      );
+    }
   }
 };
 </script>
 
 <style type="sass" scoped>
-.container {
+.chips-container {
+  align-items: center;
+  padding-bottom: 16px;
+  padding-right: 16px;
+  padding-left: 16px;
+}
+.cards-container {
   display: inline-grid;
   grid-row-gap: 32px;
   grid-template-columns: auto auto auto;
