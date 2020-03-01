531c9ba86b78ebb0f37c49c0707a5068f34daf16
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-03-01 15:48:39 -0300

    Fix when display team secret key

--- frontend/src/views/AfterLogin.vue
+++ frontend/src/views/AfterLogin.vue
@@ -1,5 +1,5 @@
 <template>
-  <md-content v-if="encodedTeam && team.options == 'create'">
+  <md-content v-if="encodedTeam && team.option == 'create'">
     <p>{{ $t("encodedTeam") }}</p>
     <md-field>
       <md-textarea v-model="encodedTeam" md-autogrow disabled></md-textarea>
@@ -39,12 +39,12 @@
       :md-done="!!teamKey"
     >
       <div>
-        <md-radio v-model="team.option" value="create">
-          {{ $t("createTeam") }}
-        </md-radio>
-        <md-radio v-model="team.option" value="join">
-          {{ $t("joinTeam") }}
-        </md-radio>
+        <md-radio v-model="team.option" value="create">{{
+          $t("createTeam")
+        }}</md-radio>
+        <md-radio v-model="team.option" value="join">{{
+          $t("joinTeam")
+        }}</md-radio>
       </div>
       <div v-if="team.option === 'create'">
         <md-field>
@@ -62,17 +62,17 @@
           </md-field>
           <md-list style="height:300px; overflow:scroll">
             <md-list-item v-for="item in filteredCountries" :key="item.key">
-              <md-checkbox v-model="team.countries" :value="item.key">{{
-                item.name
-              }}</md-checkbox>
+              <md-checkbox v-model="team.countries" :value="item.key">
+                {{ item.name }}
+              </md-checkbox>
               <country-flag :country="item.key" size="normal" />
             </md-list-item>
           </md-list>
         </md-content>
         <div style="display:flex; justify-content:center;">
-          <md-button class="md-raised md-primary" @click="createTeam">
-            {{ $t("submit") }}
-          </md-button>
+          <md-button class="md-raised md-primary" @click="createTeam">{{
+            $t("submit")
+          }}</md-button>
         </div>
       </div>
       <div v-else>
@@ -81,9 +81,9 @@
           <md-input v-model="encodedTeam"></md-input>
         </md-field>
         <div style="display:flex; justify-content:center;">
-          <md-button class="md-raised md-primary" @click="joinTeam">{{
-            $t("submit")
-          }}</md-button>
+          <md-button class="md-raised md-primary" @click="joinTeam">
+            {{ $t("submit") }}
+          </md-button>
         </div>
       </div>
     </md-step>
@@ -202,6 +202,10 @@ export default {
             this.setNextStepper("team");
           })
           .catch(() => (this.errors.fork = "Try again later"));
+      } else if (!this.encodedTeam && this.teamKey) {
+        this.encodedTeam = Buffer.from(JSON.stringify(this.teamKey)).toString(
+          "base64"
+        );
       }
     },
     async getToken() {
