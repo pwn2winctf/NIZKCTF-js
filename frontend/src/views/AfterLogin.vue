f25eb39ff1e8f3ad48f3b7c3ff334198ec7e6f63
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-03-06 01:30:18 -0300

    Fix joinTeam

--- frontend/src/views/AfterLogin.vue
+++ frontend/src/views/AfterLogin.vue
@@ -39,12 +39,12 @@
       :md-done="!!teamKey"
     >
       <div>
-        <md-radio v-model="team.option" value="create">{{
-          $t("createTeam")
-        }}</md-radio>
-        <md-radio v-model="team.option" value="join">{{
-          $t("joinTeam")
-        }}</md-radio>
+        <md-radio v-model="team.option" value="create">
+          {{ $t("createTeam") }}
+        </md-radio>
+        <md-radio v-model="team.option" value="join">
+          {{ $t("joinTeam") }}
+        </md-radio>
       </div>
       <div v-if="team.option === 'create'">
         <md-field>
@@ -62,17 +62,17 @@
           </md-field>
           <md-list style="height:300px; overflow:scroll">
             <md-list-item v-for="item in filteredCountries" :key="item.key">
-              <md-checkbox v-model="team.countries" :value="item.key">
-                {{ item.name }}
-              </md-checkbox>
+              <md-checkbox v-model="team.countries" :value="item.key">{{
+                item.name
+              }}</md-checkbox>
               <country-flag :country="item.key" size="normal" />
             </md-list-item>
           </md-list>
         </md-content>
         <div style="display:flex; justify-content:center;">
-          <md-button class="md-raised md-primary" @click="createTeam">{{
-            $t("submit")
-          }}</md-button>
+          <md-button class="md-raised md-primary" @click="createTeam">
+            {{ $t("submit") }}
+          </md-button>
         </div>
       </div>
       <div v-else>
@@ -81,9 +81,9 @@
           <md-input v-model="encodedTeam"></md-input>
         </md-field>
         <div style="display:flex; justify-content:center;">
-          <md-button class="md-raised md-primary" @click="joinTeam">
-            {{ $t("submit") }}
-          </md-button>
+          <md-button class="md-raised md-primary" @click="joinTeam">{{
+            $t("submit")
+          }}</md-button>
         </div>
       </div>
     </md-step>
@@ -176,10 +176,7 @@ export default {
         .catch(err => console.error(err));
     },
     joinTeam() {
-      const team = JSON.parse(
-        JSON.parse(Buffer(this.encodedTeam, "base64").toString())
-      );
-
+      const team = JSON.parse(Buffer(this.encodedTeam, "base64").toString());
       this.setTeam(team);
       console.log("Join team", team);
     },
