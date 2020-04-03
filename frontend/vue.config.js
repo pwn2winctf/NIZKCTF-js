ea7afc735f88d0eb79660761d1e477ce3cad1b24
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-04-03 12:23:24 -0300

    Change title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,16 @@
-const config = require("./src/config.json");
+const configJson = require("./src/config.json");
 
 module.exports = {
-  publicPath: config.deployPath,
+  publicPath: configJson.deployPath,
   configureWebpack: {
     devtool: "source-map"
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = configJson.title
+        return args
+      })
   }
 };
