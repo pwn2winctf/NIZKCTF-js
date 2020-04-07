6cee2bf3024f12a32cd4f1312b30c83f543d3024
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-04-06 23:07:49 -0300

    Configure sentry

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,12 +1,20 @@
 const { version } = require("./package.json");
 const configJson = require("./src/config.json");
+const SentryPlugin = require("@sentry/webpack-plugin");
 
 process.env.VUE_APP_VERSION = version;
 
 module.exports = {
   publicPath: configJson.deployPath,
   configureWebpack: {
-    devtool: "source-map"
+    devtool: "source-map",
+    plugins: [
+      new SentryPlugin({
+        release: `web-client@${process.env.VUE_APP_VERSION}`,
+        include: "./dist",
+        ignore: ["node_modules", "vue.config.js"]
+      })
+    ]
   },
   chainWebpack: config => {
     config.plugin("html").tap(args => {
